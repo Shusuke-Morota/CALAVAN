f568a4deeac1c8c31a592669589331a2f7268d8d
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-05-10 22:55:04 +0900

    [5章start]

---
+++ db/migrate/20200510104329_add_title_to_blogs.rb
@@ -0,0 +1,7 @@
+class AddTitleToBlogs < ActiveRecord::Migration[5.2]
+  def change
+    add_column :blogs, :title, :string
+    add_column :blogs, :category, :string
+    add_column :blogs, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_013501) do
+ActiveRecord::Schema.define(version: 2020_05_10_104329) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "category"
+    t.text "body"
   end
 
 end
